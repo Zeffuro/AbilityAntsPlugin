a8802a6c98bb30fabd434f351e95be8f47d245f1
Zeffuro <Jeffroiscool@gmail.com>
2025-03-26 17:54:09 +0100

    API12

--- AbilityAntsPlugin/AbilityAntsAddressResolver.cs
+++ AbilityAntsPlugin/AbilityAntsAddressResolver.cs
@@ -10,7 +10,7 @@ namespace AbilityAntsPlugin
         public static void Setup64Bit(ISigScanner scanner)
         {
             // TODO: Swap to hook from IsActionHighlighted in CS
-            ShouldDrawAnts = scanner.ScanText("E8 ?? ?? ?? ?? 88 46 41 80 BF");
+            ShouldDrawAnts = scanner.ScanText("E8 ?? ?? ?? ?? 88 47 ?? 80 BB");
         }
     }
 }
